369fe4a0c571085bde16d207f52b52e4f71ec5e6
frankye961 <strikeaglef15d@gmail.com>
2025-09-06 00:58:37 +0200

    cleaning comments

--- src/main/java/com/smartchat/gateway/configuration/SecurityConfig.java
+++ src/main/java/com/smartchat/gateway/configuration/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
                 .build();
     }
 
-    // THEN: protect everything else with JWT
     @Bean
     @Order(0)
     SecurityWebFilterChain appChain(ServerHttpSecurity http) {
